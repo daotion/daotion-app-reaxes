7186705b01b4f6c5b7edadae295cf658cebc1ff9
Ferry <liqunzhang3@gmail.com>
2022-07-04 19:18:23 +0800

    uI : 完成Daotion-pop

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -5,8 +5,9 @@ import { SignTest } from './Sign';
 import {DxzPluginList} from './dxz-plugin-list';
 import { UseSyncExternalStore } from './useSyncExternalStore';
 import {TextWallet} from './reax-wallet';
-import{DxzTokenOverview}from'./dxz-Token-overview'
-const C = DxzTokenOverview;
+import{DxzTokenOverview}from'./dxz-Token-overview';
+import{DxzDaotionPop}from'./dxz-Daotion-pop'
+const C = DxzDaotionPop;
 
 
 export const Test = () => <C/>
