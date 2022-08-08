50c5bbea28ebf728804095e4a93da3c33618ded1
Ferry <liqunzhang3@gmail.com>
2022-08-08 16:45:44 +0800

    no message

--- src/pages/Test/dxz-create-SBT/index.tsx
+++ src/pages/Test/dxz-create-SBT/index.tsx
@@ -1,6 +1,10 @@
-import less from './index.module.less'
-export const DxzCreateSBT=()=>{
-	return<>
+export const DxzCreateSBT = () => {
+	return <>
 	
-	</>
-}
+	</>;
+};
+import less from './index.module.less';
+import {
+	PrimaryInput ,
+	PrimaryTextArea,
+} from "@@pages/Test/dxz-input";
