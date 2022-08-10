0d3d6493613e386bd795b75b606911fbe18d2a98
Ferry <liqunzhang3@gmail.com>
2022-08-10 18:06:40 +0800

    create space modal优化

--- src/reaxels/Spaces/create-space.tsx
+++ src/reaxels/Spaces/create-space.tsx
@@ -173,6 +173,7 @@ export const reaxel_create_space = function () {
 							onCancel = { () => setState( { visible : false } ) }
 							footer = { <>
 								<Button
+									className={less.createSpaceBtn}
 									onClick = { () => {
 										if ( !validations.input_email_address( store.input_email ) ) {
 											return;
@@ -181,21 +182,6 @@ export const reaxel_create_space = function () {
 											return;
 										}
 										create_space();
-									} }
-									style = { {
-										display : "flex" ,
-										alignItems : "center" ,
-										justifyContent : "center" ,
-										height : '56px' ,
-										width : "100%" ,
-										borderRadius : "12px" ,
-										background : "#0070f3" ,
-										color : "#ffffff" ,
-										fontSize : "15px" ,
-										fontWeight : "700" ,
-										lineHeight : "24px" ,
-										marginTop : '24px' ,
-										border : "none" ,
 									} }
 								>
 									<I18n>
@@ -272,7 +258,6 @@ export const reaxel_create_space = function () {
 										<MultipleSelect
 											type='primary'
 											suffixIcon = { <SVGSelectArrowIcon /> }
-											removeIcon = { <SVGClear /> }
 											mode = "tags"
 											dropdownMatchSelectWidth = { true }
 											placeholder = { i18n( "Please select" ) }
@@ -339,110 +324,11 @@ export const reaxel_create_space = function () {
 		};
 	};
 }();
-const SVGCloseIcon = () => {
-	return <>
-		<svg
-			width = "40"
-			height = "40"
-			viewBox = "0 0 40 40"
-			fill = "none"
-			xmlns = "http://www.w3.org/2000/svg"
-		>
-			<path
-				fillRule = "evenodd"
-				clipRule = "evenodd"
-				d = "M13.2929 13.2929C13.6834 12.9024 14.3166 12.9024 14.7071 13.2929L20 18.5858L25.2929 13.2929C25.6834 12.9024 26.3166 12.9024 26.7071 13.2929C27.0976 13.6834 27.0976 14.3166 26.7071 14.7071L21.4142 20L26.7071 25.2929C27.0976 25.6834 27.0976 26.3166 26.7071 26.7071C26.3166 27.0976 25.6834 27.0976 25.2929 26.7071L20 21.4142L14.7071 26.7071C14.3166 27.0976 13.6834 27.0976 13.2929 26.7071C12.9024 26.3166 12.9024 25.6834 13.2929 25.2929L18.5858 20L13.2929 14.7071C12.9024 14.3166 12.9024 13.6834 13.2929 13.2929Z"
-				fill = "#313436"
-			/>
-			<rect
-				x = "1"
-				y = "1"
-				width = "38"
-				height = "38"
-				rx = "19"
-				stroke = "#E6E8EC"
-				strokeWidth = "2"
-			/>
-		</svg>
-	</>;
-};
+
 import { PrimaryInput } from '@@pages/Test/dxz-input';
 import { MultipleSelect } from '@@pages/Test/dxz-select';
 import less from '../../styles/reaxels.module.less';
-import { SVGClear } from '@@pages/_SvgComponents/space-setting-svg';
+import { SVGSelectArrowIcon, SVGCloseIcon} from '@@pages/_SvgComponents/space-setting-svg';
+import { OptionNetEthereum } from '@@pages/Test/dxz-create-SBT';
 
-const SVGSelectArrowIcon = () => {
-	return <>
-		<svg
-			width = "24"
-			height = "24"
-			viewBox = "0 0 24 24"
-			fill = "none"
-			xmlns = "http://www.w3.org/2000/svg"
-		>
-			<path
-				d = "M6.70711 8.29289C6.31658 7.90237 5.68342 7.90237 5.29289 8.29289C4.90237 8.68342 4.90237 9.31658 5.29289 9.70711L6.70711 8.29289ZM12 15L11.2929 15.7071C11.6834 16.0976 12.3166 16.0976 12.7071 15.7071L12 15ZM18.7071 9.70711C19.0976 9.31658 19.0976 8.68342 18.7071 8.29289C18.3166 7.90237 17.6834 7.90237 17.2929 8.29289L18.7071 9.70711ZM5.29289 9.70711L11.2929 15.7071L12.7071 14.2929L6.70711 8.29289L5.29289 9.70711ZM12.7071 15.7071L18.7071 9.70711L17.2929 8.29289L11.2929 14.2929L12.7071 15.7071Z"
-				fill = "#6F767E"
-			/>
-		</svg>
-	</>;
-};
-export const OptionNetEthereum = ComponentWrapper( () => {
-	return <>
-		<span className={less.netEthereum}>
-			<SVGNetEthereum/>
-			Ethereum
-		</span>
-	</>;
-} );
-export const SVGNetEthereum = ComponentWrapper( () => {
-	return <>
-		<svg
-			style={{marginRight:"8px"}}
-			width = "24"
-			height = "24"
-			viewBox = "0 0 16 16"
-			fill = "none"
-			xmlns = "http://www.w3.org/2000/svg"
-		>
-			<g clipPath = "url(#clip0_2540_29067)">
-				<path
-					d = "M0 8C0 3.5816 3.58166 0 8.00014 0C12.4186 0 16.0003 3.5816 16.0003 8C16.0003 12.4184 12.4186 16 8.00014 16C3.58166 16 0 12.4184 0 8Z"
-					fill = "#627EEA"
-				/>
-				<path
-					d = "M4 7.7243L8 9.99967V1.33301L4 7.7243Z"
-					fill = "white"
-				/>
-				<path
-					opacity = "0.8"
-					d = "M8.00041 1.33301L8 9.99967L12 7.75276L8.00041 1.33301Z"
-					fill = "#C0CBF6"
-				/>
-				<path
-					d = "M4 8.86426L7.92927 14.4003V11.1859L4 8.86466V8.86426Z"
-					fill = "white"
-				/>
-				<path
-					opacity = "0.8"
-					d = "M7.92969 11.1859V14.4003L11.8606 8.86426L7.92969 11.1859Z"
-					fill = "#C0CBF6"
-				/>
-				<path
-					opacity = "0.6"
-					d = "M8.00061 6L4 7.73966L8.00061 10L12 7.73966L8.00061 6Z"
-					fill = "#8197EE"
-				/>
-			</g>
-			<defs>
-				<clipPath id = "clip0_2540_29067">
-					<rect
-						width = "16.0003"
-						height = "16"
-						fill = "white"
-					/>
-				</clipPath>
-			</defs>
-		</svg>
-	</>;
-} );
+
