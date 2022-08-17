8a50802de8dbe23783e023f708f5a4f140732cce
Ferry <liqunzhang3@gmail.com>
2022-08-17 15:28:41 +0800

    tags-select-组件完成

--- src/pages/Test/dxz-select/index.tsx
+++ src/pages/Test/dxz-select/index.tsx
@@ -8,7 +8,25 @@ for ( let i = 10 ; i < 36 ; i++ ) {
 const handleChange = ( value : string[] ) => {
 	console.log( `selected ${ value }` );
 };
-
+export const TagsSelect = ( props
+	// :SelectProps&{
+	// type : "primary"} 
+) => {
+	return <Select
+		className = { less.tagsPrimary }
+		dropdownClassName = { less.dropDownMenu }
+		mode = "tags"
+		allowClear
+		style = { { width : '100%' } }
+		// showArrow
+		removeIcon = { <SVGTagsClear /> }
+		placeholder = { i18n( "Please select" ) }
+		{ ...props }
+	>
+		{props.children}
+	</Select>;
+	
+};
 export const MultipleSelect = ( props:SelectProps&{
 	type : "primary"
 } ) => {
@@ -53,9 +71,17 @@ export const DxzSelect = ComponentWrapper( () => {
 			<SingleSelect
 			type='single-primary'
 			/>
+			<TagsSelect/>
 		</div>
 	</>;
 } );
 import less from './index.module.less';
 import { Select , SelectProps } from 'antd';
 import { SVGClear } from '@@pages/_SvgComponents/space-setting-svg';
+export const SVGTagsClear=ComponentWrapper(()=>{
+	return<>
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M6.7072 5.29289C6.31668 4.90237 5.68351 4.90237 5.29299 5.29289C4.90246 5.68342 4.90246 6.31658 5.29299 6.70711L10.5858 12L5.29289 17.2929C4.90237 17.6834 4.90237 18.3166 5.29289 18.7071C5.68342 19.0976 6.31658 19.0976 6.70711 18.7071L12 13.4142L17.293 18.7071C17.6835 19.0976 18.3167 19.0976 18.7072 18.7071C19.0977 18.3166 19.0977 17.6834 18.7072 17.2929L13.4143 12L18.7071 6.70711C19.0976 6.31658 19.0976 5.68342 18.7071 5.29289C18.3166 4.90237 17.6834 4.90237 17.2929 5.29289L12 10.5857L6.7072 5.29289Z" fill="#FCFCFC"/>
+		</svg>
+	</>
+})
