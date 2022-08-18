780d15ee1e272b0fb0a6797a5a6f8753d755827a
Ferry <liqunzhang3@gmail.com>
2022-08-18 10:27:00 +0800

    fix

--- src/pages/Test/dxz-New-SBT/index.tsx
+++ src/pages/Test/dxz-New-SBT/index.tsx
@@ -136,30 +136,7 @@ export const DxzCreateSBT = ComponentWrapper( () => {
 					</div>
 				</div>
 				{/*右边的preview部分 :*/ }
-				<div className = { less.createSBT_right }>					
-					<span className = { less.createSBTPreviewTitle }>
-						<I18n>Preview</I18n>
-					</span>
-					
-					{ file ? <div>
-						<Img
-							className = { less.SBTPreviewImg }
-							src = {imgPreviewUrl}
-						/>
-						<div className = { less.SBTPreviewName }>
-							<I18n>{newSBT_store.input__SBT_name}</I18n>
-						</div>
-						<div className = { less.SBTPreviewSpace }>
-							<Img
-								className = { less.previewSpaceAvatar }
-								src = ""
-							/>
-							<span>SpaceName</span>
-						</div>
-					</div> : <p className = { less.createSBTPreviewContent }>
-						Upload file and choose collection to preview your brand new NFT
-					</p> }
-				</div>
+				<NewSBTPreview/>
 			</div>
 		</div>
 	</>;
@@ -200,7 +177,44 @@ import {
 	SVGUploadFileIcon ,
 } from '@@pages/_SvgComponents/all-SBT-SVG';
 
-
+export const NewSBTPreview=ComponentWrapper(()=>{
+	const reax_newSBT = reaxel_newSBT();
+	const {
+		file,
+		imgPreviewUrl
+	} = reaxel_DDF();
+	const {
+		createSBT ,
+		enum__SBT_type ,
+		newSBT_store ,
+		setFields ,
+	} = reax_newSBT;
+	return<>
+		<div className = { less.createSBT_right }>
+			<span className = { less.createSBTPreviewTitle }>
+				<I18n>Preview</I18n>
+			</span>
+			
+			{ file ? <div>
+				<Img
+					className = { less.SBTPreviewImg }
+					src = {imgPreviewUrl}
+				/>
+				<div className = { less.SBTPreviewName }>
+					<I18n>{newSBT_store.input__SBT_name}</I18n>
+				</div>
+				<div className = { less.SBTPreviewSpace }>
+					<Img
+						className = { less.previewSpaceAvatar }
+						src = ""
+					/>
+					<span>SpaceName</span>
+				</div>
+			</div> : <p className = { less.createSBTPreviewContent }>
+				Upload file and choose collection to preview your brand new NFT
+			</p> }
+		</div></>
+})
 export const CreateSBTCheckBox = ( props ) => {
 	return <>
 		<div className = { less.createSBTCheckBox }>
