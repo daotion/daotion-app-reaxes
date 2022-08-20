7c4adb7424cf2baf43fc5e4b403702957c38c0aa
Kane <18299687659@163.com>
2022-08-20 17:05:55 +0800

    validation-chainReactionMode

--- src/pages/Test/Validation-Fields/index.tsx
+++ src/pages/Test/Validation-Fields/index.tsx
@@ -2,65 +2,79 @@
 export const Validations = ComponentWrapper( () => {
 	const {
 		Input ,
-		Select,
+		Select ,
+		Switch,
 	} = antd;
 	const {
 		input ,
 		select,
+		checked ,
 		validate ,
 		setFields ,
 		resetValidation,
 		validations,
 	} = reaxel_validate_fields();
-	console.log( 1111111111 );
+	console.log( [input , select , checked ]);
+	console.log( validations);
 	return <>
 		<div
-			style = {{
-				
-			}}
+			style = { {} }
 		>
 			<Input
 				value = { input }
 				size = "large"
-				status = {convert(validations.input)}
+				status = { convert( validations.input ) }
 				onInput = { ( e ) => setFields( { input : e.target.value } ) }
 			/>
-			{validations.input === false && <p
-				style={{
-					color : "red",
-				}}
-			>输入不合法!</p>}
+			{ validations.input === false && <p
+				style = { {
+					color : "red" ,
+				} }
+			>输入不合法!</p> }
 			<Select
 				value = { select }
 				size = "large"
-				status = {convert(validations.select)}
-				onChange = {(value) => {
+				status = { convert( validations.select ) }
+				onChange = { ( value ) => {
 					setFields( { select : value } );
-				}}
+				} }
 			>
-				<Select.Option value="opt1">opt1</Select.Option>				
-				<Select.Option value="opt2">opt2</Select.Option>				
-				<Select.Option value="opt3">opt3</Select.Option>				
+				<Select.Option value = "opt1">opt1</Select.Option>
+				<Select.Option value = "opt2">opt2</Select.Option>
+				<Select.Option value = "opt3">opt3</Select.Option>
 			</Select>
+			<Switch
+				checked = { checked }
+				onChange = { ( checked ) => setFields( { checked } ) }
+			/>
+			{ validations.switch === false && <p
+				style = { {
+					color : "red" ,
+				} }
+			>输入不合法!</p> }
 		</div>
-	
+		
 		<Button>submit</Button>
 		<Button
-			onClick={() => {
+			onClick={validate}
+		>force validate</Button>
+		<Button
+			onClick = { () => {
 				resetValidation();
-			}}
+			} }
 		>Reset Validate Status</Button>
-	</>
+	</>;
 } );
-import { reaxel_fact__validation } from './Input';
+import { reaxel_fact__validation } from './reaxel-fact--validations';
 
 
 
 const reaxel_validate_fields = function(){
-	
+	let ret;
 	const {store,setState} = orzMobx({
 		input: null ,
 		select : null ,
+		checked : false ,
 	})
 	const validates = {
 		input : async (value) => {
@@ -78,24 +92,38 @@ const reaxel_validate_fields = function(){
 				return true;
 			}
 		},
+		switch : (checked:boolean ) => {
+			console.log( checked );
+			if(!reaxel_validation_Input(store.input).valid ||
+				!reaxel_validation_Select(store.select).valid){
+				return false;
+			}else {
+				return checked === false;
+			}
+		},
 	};
 	
 	const reaxel_validation_Input = reaxel_fact__validation( validates.input );
 	const reaxel_validation_Select = reaxel_fact__validation( validates.select );
+	const reaxel_validation_Switch = reaxel_fact__validation( validates.switch , true );
 	
 	return () => {
 		
-		return {
+		return ret = {
 			get input(){
 				return store.input;
 			},
 			get select(){
 				return store.select;
 			},
+			get checked(){
+				return store.checked;
+			},
 			get validations() {
 				return {
 					input : reaxel_validation_Input(store.input).valid,
 					select : reaxel_validation_Select(store.select).valid,
+					switch : reaxel_validation_Switch(store.checked).valid,
 				};
 			},
 			get validators() {
@@ -107,10 +135,12 @@ const reaxel_validate_fields = function(){
 			validate(){
 				reaxel_validation_Input(store.input).validate();
 				reaxel_validation_Select( store.select ).validate();
+				reaxel_validation_Switch(store.checked).validate();
 			},
 			resetValidation(){
 				reaxel_validation_Input(store.input).reset();
 				reaxel_validation_Select( store.select ).reset();
+				reaxel_validation_Switch( store.checked ).reset();
 			},
 		};
 	};
--- src/pages/Test/Validation-Fields/reaxel-fact--validations.tsx
+++ src/pages/Test/Validation-Fields/reaxel-fact--validations.tsx
@@ -1,4 +1,10 @@
-export const reaxel_fact__validation = function(validator){
+/**
+ * 
+ * @param validator 
+ * @param chainReactionMode 连锁模式: 这个当其他字段改变时
+ * 就算这个字段没发生改变也会触发校验和视图更新
+ */
+export const reaxel_fact__validation = function(validator , chainReactionMode = false ){
 	
 	const {
 		store ,
@@ -12,6 +18,8 @@ export const reaxel_fact__validation = function(validator){
 	let fieldChanged = false;
 	let _value = Symbol();
 	let pending = null;
+	
+	
 	return (value) => {
 		
 		
@@ -20,7 +28,7 @@ export const reaxel_fact__validation = function(validator){
 				if(first){
 					first = false;
 					_value = value;
-				}else if(_value !== value) {
+				}else if(chainReactionMode || _value !== value) {
 					const res = pending = validator(value);
 					if(res.then && res.catch){
 						res.then( ( validation ) => {
@@ -52,14 +60,32 @@ export const reaxel_fact__validation = function(validator){
 				} );
 			},
 			validate(){
-				const res = validator(value);
+				first = false;
+				const res = pending = validator(value);
 				if(res.then && res.catch){
 					res.then( ( validation ) => {
-						setState( { valid : validation } );
+						/*如果中途再次触发了校验,则取消之前校验的结果,只取最后一次生效的promise*/
+						if(pending !== null && pending !== res){
+							return;
+						}
+						setState( { valid : true } );
+					} );
+					res.catch( () => {
+						if ( pending !== null && pending !== res ) {
+							return;
+						}
+						setState( { valid : false } );
+					} );
+					res.finally( () => {
+						if(pending === res) pending = null;
+						
 					} );
 				}else {
-					queueMicrotask( () => setState( { valid : res } ) );
+					queueMicrotask( () => {
+						setState( { valid : res } );
+					} );
 				}
+				_value = value;
 			},
 		};
 	}
