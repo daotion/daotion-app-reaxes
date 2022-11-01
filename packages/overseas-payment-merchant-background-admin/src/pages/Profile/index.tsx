aec0634f1c14a73cba0253ae9f5edc278864a702
mozi <mozi0309@gmail.com>
2022-11-01 16:50:38 +0800

    feat: api修改

--- packages/overseas-payment-merchant-background-admin/src/pages/Profile/index.tsx
+++ packages/overseas-payment-merchant-background-admin/src/pages/Profile/index.tsx
@@ -66,9 +66,6 @@ export const ResetPwd = reaxper(() =>{
 				<span className={less.title}>
 					修改密码
 				</span>
-				<span className={less.hint}>
-					密码修改成功后需重新登录
-				</span>
 			</div>
 			<div className={less.formContainer}>
 				<span className={less.formTitle}>
@@ -183,70 +180,135 @@ export const ProfileApi = reaxper(() => {
 	const badge = useRef(Math.random())
 	const { Space, Col, Row, Button } = antd;
 	const {closuredFetchApiConfig,apiConfig} = reaxel_user_info();
-	
 	closuredFetchApiConfig(badge.current);
-	
+	const reax_edit_info = reaxel_edit_info();
 	const {
 		mchKey = '' ,
 		platformIPS = '' ,
 		payInCallback = '' ,
 		payOutCallback = '' ,
 		payOutWhitelist = [] ,
-		withdrawAdd = '' ,
+		address = '' ,
 	} = apiConfig;
+
 	return (
-		<div className = { less.baseInfo }>
-			<div className={less.baseInfoTitle}>
-				API对接
+		<>
+			<div className = { less.baseInfo }>
+				<div className={less.baseInfoTitle}>
+					API对接
+				</div>
+				<div
+					className = { less.infoItem }
+					style = { { marginTop : 0 } }
+				>
+					<Col span = { 6 }>商户Key：</Col>
+					<Col style={{
+						display: 'flex',
+						alignItems: 'center'
+					}}>{ mchKey }<CopyBtn/></Col>
+				</div>
+				<div className = { less.infoItem }>
+					<Col span = { 6 }>平台IP：</Col>
+					<Col>{ platformIPS }</Col>
+				</div>
+				<div className = { less.infoItem }>
+					<Col span = { 6 }>代收回调url：</Col>
+					<Col span = { 6 }>{ payInCallback }</Col>
+					<Col>
+						<Button type="link" onClick={() => {reax_edit_info.showModal('payInCallback')}}>设置</Button>
+					</Col>
+				</div>
+				<div className = { less.infoItem }>
+					<Col span = { 6 }>代付回调url：</Col>
+					<Col span = { 6 }>{ payOutCallback }</Col>
+					<Col>
+						<Button type="link" onClick={() => {reax_edit_info.showModal('payOutCallback')}}>设置</Button>
+					</Col>
+				</div>
+				<div className = { less.infoItem }>
+					<Col span = { 6 }>
+						<p>代付白名单IP：</p>
+						<p className={less.declare}>如果有设置，平台只接收来自白名单ip地址的代付请求。</p>
+					</Col>
+					<Col span = { 6 }>{ payOutWhitelist.map((i) => <span key = { i }>{ i};</span>) }</Col>
+					<Col>
+						<Button type="link" onClick={() => {reax_edit_info.showModal('payOutWhitelist')}}>设置</Button>
+					</Col>
+				</div>
+				<div className = { less.infoItem }>
+					<Col span = { 6 }>提现地址(TRC-20)：</Col>
+					<Col span = { 6 }>{ address }</Col>
+					<Col>
+						<Button type="link" onClick={() => {reax_edit_info.showModal('address')}}>设置</Button>
+					</Col>
+				</div>
 			</div>
-			<div
-				className = { less.infoItem }
-				style = { { marginTop : 0 } }
-			>
-				<Col span = { 6 }>商户Key：</Col>
-				<Col style={{
-					display: 'flex',
-					alignItems: 'center'
-				}}>{ mchKey }<CopyBtn/></Col>
-			</div>
-			<div className = { less.infoItem }>
-				<Col span = { 6 }>平台IP：</Col>
-				<Col>{ platformIPS }</Col>
-			</div>
-			<div className = { less.infoItem }>
-				<Col span = { 6 }>代收回调url：</Col>
-				<Col span = { 6 }>{ payInCallback }</Col>
-				<Col>
-					<Button type="link">设置</Button>
-				</Col>
-			</div>
-			<div className = { less.infoItem }>
-				<Col span = { 6 }>代付回调url：</Col>
-				<Col span = { 6 }>{ payOutCallback }</Col>
-				<Col>
-					<Button type="link">设置</Button>
-				</Col>
-			</div>
-			<div className = { less.infoItem }>
-				<Col span = { 6 }>
-					<p>代付白名单IP：</p>
-					<p className={less.declare}>如果有设置，平台只接收来自白名单ip地址的代付请求。</p>
-				</Col>
-				<Col span = { 6 }>{ payOutWhitelist.map((i) => <span key = { i }>{ i};</span>) }</Col>
-				<Col>
-					<Button type="link">设置</Button>
-				</Col>
-			</div>
-			<div className = { less.infoItem }>
-				<Col span = { 6 }>提现地址(TRC-20)：</Col>
-				<Col span = { 6 }>{ withdrawAdd }</Col>
-				<Col>
-					<Button type="link">设置</Button>
-				</Col>
-			</div>
-		</div>
+			<SetApiModal/>
+		</>
 	);
 });
+
+const SetApiModal = reaxper(() => {
+	const { Modal, Input, Button } = antd;
+	const reax_edit_info = reaxel_edit_info();
+	const { setApiStore } = reax_edit_info;
+	const {
+		apiSetModalKey = '',
+	} = setApiStore;
+	const modalContent = {
+		'payInCallback' : {
+			title : '设置代收回调url' ,
+			subTitle : '代收回调 url' ,
+			value : setApiStore.payInCallback,
+			
+		},
+		'payOutCallback' : {
+			title : '设置代付回调url' ,
+			subTitle : '代付回调url' ,
+			value : setApiStore.payOutCallback,
+		},
+		'payOutWhitelist' : {
+			title : '设置代付白名单 ' ,
+			subTitle : '代付白名单IP ' ,
+			memo : '如多个IP,每个IP之间用英文;隔开。如果有设置，平台只接收来自白名单中的IP地址的代付请求。' ,
+			value : setApiStore.payOutWhitelist,
+		},
+		'address' : {
+			title : '设置提现地址' ,
+			subTitle : 'TRC-20地址' ,
+			memo : '请确保输入正确的地址',
+			value : setApiStore.address,
+		},
+		
+	}
+	return (
+		<Modal
+			visible = { setApiStore.apiSetModalShow }
+			title = { modalContent[apiSetModalKey].title }
+			footer = { null }
+			closable={false}
+		>
+			<p>{ modalContent[apiSetModalKey].subTitle }</p>
+			<span>{ modalContent[apiSetModalKey].memo }</span>
+			<Input
+				value = {modalContent[apiSetModalKey].value}
+				onChange={(e) => {
+					reax_edit_info.setStateApi({
+						[apiSetModalKey]: e.target.value
+					})
+				}}
+			/>
+			<Button type = "primary" onClick={() => {
+				reax_edit_info.setApiConfig()
+			}}>提交</Button>
+			<Button onClick={() => {
+				reax_edit_info.setStateApi({
+					apiSetModalShow: false
+				})
+			}}>取消</Button>
+		</Modal>
+	);
+})
 
 
 
