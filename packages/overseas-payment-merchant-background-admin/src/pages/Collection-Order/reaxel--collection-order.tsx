38a98331f6da2c0a6c97f2a20cda0ad127c3c01a
Kane <18299687659@163.com>
2022-11-07 15:43:56 +0800

    depoist completed

--- packages/overseas-payment-merchant-background-admin/src/pages/Collection-Order/reaxel--collection-order.tsx
+++ packages/overseas-payment-merchant-background-admin/src/pages/Collection-Order/reaxel--collection-order.tsx
@@ -15,10 +15,9 @@ export const reaxel_collection_order = function(){
 	const { store : store$search , setState : setState$search } = orzMobx(initialSearch);
 	const { store : store$orderList , setState : setState$orderList } = orzMobx(initialOrderList);
 	
-	const {store, setState} = orzMobx({
-		processModalShow: false
-		
-	})
+	const { store , setState } = orzMobx({
+		processModalShow : false ,
+	});
 	
 	const setPending = (pending) => {
 		// setState$orderList({ pending })
@@ -29,7 +28,7 @@ export const reaxel_collection_order = function(){
 			"collection-order" : request_collection_order,
 			"payment-order" : request_payment_order,
 			"withdrawal-order" : request_withdrawal_order,
-			"deposit-order" : request_withdrawal_order,
+			"deposit-order" : request_deposit_order,
 		};
 		setPending(true);
 		return fetchMap[path](async () => {
@@ -62,19 +61,11 @@ export const reaxel_collection_order = function(){
 	const [closuredFetch , clearDeps] = Reaxes.closuredMemo((path) => {
 		fetchCollectionOrder(path);
 	} , () => []);
-
+	/*记录当前是什么订单列表,如果和当前请求的列表不匹配collection_order_list就会返回空*/
 	let currentPath;
 	return (path) => {
 		
 		return ret = {
-			get_enum_order_list_map(path){
-				return {
-					"collection-order" : enum_collection_order_status,
-					"payment-order" : enum_payment_order_status,
-					"withdrawal-order" : enum_withdrawal_order_status,
-					"deposit-order" : enum_deposit_order_status,
-				}[path];
-			},
 			get state$search(){
 				return store$search;
 			},
@@ -94,8 +85,19 @@ export const reaxel_collection_order = function(){
 			get setFields(){
 				return setState$search;
 			},
+			get processModalShow(){
+				return store.processModalShow;
+			},
+			get_enum_order_list_map(path){
+				return {
+					"collection-order" : enum_collection_order_status,
+					"payment-order" : enum_payment_order_status,
+					"withdrawal-order" : enum_withdrawal_order_status,
+					"deposit-order" : enum_deposit_order_status,
+				}[path];
+			},
 			fetchCollectionOrderList(path){
-				return closuredFetch(() => [ 
+				return closuredFetch(() => [
 					store$search.input_search_orderID ,
 					store$search.select_order_status ,
 					path 
@@ -103,9 +105,6 @@ export const reaxel_collection_order = function(){
 			},
 			resetSearch(){
 				setState$search(initialSearch);
-			},
-			get processModalShow(){
-				return store.processModalShow;
 			},
 			resetFetchDeps(){
 				clearDeps();
@@ -126,6 +125,7 @@ import {
 	Order__collection_order ,
 	request_payment_order ,
 	request_withdrawal_order,
+	request_deposit_order,
 } from '@@requests';
 import { reaxel_fact__prevent_dup_request } from '#reaxels';
 
