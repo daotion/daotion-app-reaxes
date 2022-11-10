851ceb1b9c21326d8e30d0b1305da08cd7dfd030
mozi <mozi0309@gmail.com>
2022-11-10 10:39:24 +0800

    feat：overview骨架屏

--- packages/overseas-payment-merchant-background-admin/src/pages/--Components--/Overview/overview-info.tsx
+++ packages/overseas-payment-merchant-background-admin/src/pages/--Components--/Overview/overview-info.tsx
@@ -4,75 +4,87 @@ export const OverviewInfo = reaxper(() => {
 	
 	const { Button } = antd;
 	const { navigate } = toolkits.useRouter();
-	const { overviewInfo , fetchOverviewInfo } = reaxel_overview_info();
+	const { overviewInfo , fetchOverviewInfo, overviewInfoPending } = reaxel_overview_info();
 	const {
 		balance = 0 ,
 		withdrawingMoney = 0,
 	} = overviewInfo;
 	const { current : badge } = useRef(Math.random());
-	fetchOverviewInfo(badge);
+	useEffect(() => {
+		fetchOverviewInfo()
+	},[])
 	return (
-		<div className = { less.overviewContainer }>
-			<span className = { less.overviewTitle }>
-				资金总览
-			</span>
-			<div className = { less.overviewContent }>
-				<div className = { less.balanceContainer }>
-					<div className = { less.balanceContent }>
+		<div
+			style={{
+				padding: '24px',
+				width: '100%',
+				backgroundColor: '#FFFFFF',
+				marginBottom: '20px',
+				borderRadius: '8px',
+			}}
+		>
+			{overviewInfoPending && <OverviewInfoSkeleton/>}
+			{!overviewInfoPending && <div className = { less.overviewContainer }>
+				<span className = { less.overviewTitle }>
+					资金总览
+				</span>
+				<div className = { less.overviewContent }>
+					<div className = { less.balanceContainer }>
+						<div className = { less.balanceContent }>
+							<span className = { less.overviewSubTitle }>
+								账户余额（R$）
+							</span>
+							<span className = { less.balanceAmount }>
+								{ balance }
+							</span>
+						</div>
+						<Button
+							className = { less.checkDetail }
+							type = "link"
+							onClick = { () => {
+								navigate('fin-detail');
+							} }
+						>
+							查看明细
+						</Button>
+					</div>
+					<div className = { less.withdrawContent }>
 						<span className = { less.overviewSubTitle }>
-							账户余额（R$）
+							提现处理中
 						</span>
-						<span className = { less.balanceAmount }>
-							{balance}
+						<span className = { less.withdrawingAmount }>
+							{ withdrawingMoney }
 						</span>
 					</div>
+				</div>
+				<div style = { { display : 'flex' , gap : '16px' } }>
 					<Button
-						className = { less.checkDetail }
-						type = "link"
+						type = "primary"
 						onClick = { () => {
-							navigate('fin-detail');
+							navigate('deposit');
 						} }
 					>
-						查看明细
+						充值
+					</Button>
+					<Button
+						type = "primary"
+						onClick = { () => {
+							navigate('withdraw');
+						} }
+					>
+						提现
 					</Button>
 				</div>
-				<div className = { less.withdrawContent }>
-					<span className = { less.overviewSubTitle }>
-						提现处理中
-					</span>
-					<span className = { less.withdrawingAmount }>
-						{withdrawingMoney}
-					</span>
-				</div>
-			</div>
-			<div style={{ display : 'flex', gap: '16px'}}>
-				<Button
-					type = "primary"
-					onClick = { () => {
-						navigate('deposit')
-					} }
-				>
-					充值
-				</Button>
-				<Button
-					type = "primary"
-					onClick = { () => {
-						navigate('withdraw')
-					} }
-				>
-					提现
-				</Button>
-			</div>
+			</div> }
+		
 		</div>
 	);
 });
 
 
 export const OverviewInfoSkeleton = reaxper(() => {
-	
 	const {Skeleton} = antd;
-	
-	return<>
+	return(
 		<div style={{
 			display: "flex",
 			flexDirection: "column",
@@ -87,7 +99,7 @@ export const OverviewInfoSkeleton = reaxper(() => {
 			<Skeleton.Button style={{width: 100}} active/>
 			</div>
 		</div>
-	</>
+	)
 })
 
 import { reaxel_overview_info } from "@@reaxels";
