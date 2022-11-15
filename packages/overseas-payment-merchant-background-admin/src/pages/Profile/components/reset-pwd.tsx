4326ae49275c45678f0322b4df46f5e3e27f5d6d
Frank <iamfeifan98@gmail.com>
2022-11-15 17:45:27 +0800

    Revert "修改密码页面DOM结构优化&CSS优化调整"
    This reverts commit 2823bba4af805c00c4d268048d7c36c07910bf34.

--- packages/overseas-payment-merchant-background-admin/src/pages/Profile/components/reset-pwd.tsx
+++ packages/overseas-payment-merchant-background-admin/src/pages/Profile/components/reset-pwd.tsx
@@ -12,55 +12,73 @@ export const ResetPwd = reaxper(() => {
 		} else if( resetPwdStore.newPassword !== resetPwdStore.checkPassword ) {
 			message.error('确认密码不一致');
 		} else {
-			modifyPwd().then((res) => {
-				if( res === 'error' ) {
+			modifyPwd().then((res)=>{
+				if (res === 'error') {
 					message.error(res.message || '旧密码错误');
 				} else {
-					message.success('修改成功');
+					message.success('修改成功')
 				}
-			});
+			})
 		}
 	};
 	
 	
-	const { Input , Button , message } = antd;
+	const { Input , Button,message } = antd;
 	return (
 		<div className = { less.resetPasswordContainer }>
-			<p className = { less.title }>修改密码</p>
-			<p className = { less.hint }>密码修改成功后需重新登录</p>
-			<p className = { less.formTitle }>旧密码</p>
-			<Input
-				value = { resetPwdStore.oldPassword }
-				placeholder = { '请输入旧密码' }
-				onChange = { (e) => {
-					setStatePwd({
-						oldPassword : e.target.value ,
-					});
-				} }
-				type = "password"
-			/>
-			<p className = { less.formTitle }>新密码</p>
-			<Input
-				placeholder = { '请输入新密码' }
-				value = { resetPwdStore.newPassword }
-				onChange = { (e) => {
-					setStatePwd({
-						newPassword : e.target.value ,
-					});
-				} }
-				type = "password"
-			/>
-			<p className = { less.formTitle }>再次输入密码</p>
-			<Input
-				placeholder = { '请再次输入密码' }
-				value = { resetPwdStore.checkPassword }
-				onChange = { (e) => {
-					setStatePwd({
-						checkPassword : e.target.value ,
-					});
-				} }
-				type = "password"
-			/>
+			<div className = { less.resetTitle }>
+				<span className = { less.title }>
+					修改密码
+				</span>
+				<span>
+					密码修改成功后需重新登录
+				</span>
+			</div>
+			<div className = { less.formContainer }>
+				<span className = { less.formTitle }>
+					旧密码
+				</span>
+				<Input
+					value = { resetPwdStore.oldPassword }
+					placeholder = { '请输入旧密码' }
+					onChange = { (e) => {
+						setStatePwd({
+							oldPassword : e.target.value,
+						});
+					} }
+					type = "password"
+				/>
+			</div>
+			<div className = { less.formContainer }>
+				<span className = { less.formTitle }>
+					新密码
+				</span>
+				<Input
+					placeholder = { '请输入新密码' }
+					value = { resetPwdStore.newPassword }
+					onChange = { (e) => {
+						setStatePwd({
+							newPassword : e.target.value,
+						});
+					} }
+					type = "password"
+				/>
+			</div>
+			<div className = { less.formContainer }>
+				<span className = { less.formTitle }>
+					再次输入密码
+				</span>
+				<Input
+					placeholder = { '请再次输入密码' }
+					value = { resetPwdStore.checkPassword }
+					onChange = { (e) => {
+						setStatePwd({
+							checkPassword : e.target.value,
+						});
+					} }
+					type = "password"
+				/>
+			</div>
 			<Button
 				type = "primary"
 				loading = { resetPwdStore.pending }
