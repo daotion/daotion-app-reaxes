712e274d8fd0a3a41421ccd7efaf134e16bda306
Frank <iamfeifan98@gmail.com>
2022-11-18 17:48:42 +0800

    feat: 提现申请页面弹窗静态页面

--- packages/overseas-payment-merchant-operation-platform/src/pages/Merchant-Withdraw-Rqst/index.tsx
+++ packages/overseas-payment-merchant-operation-platform/src/pages/Merchant-Withdraw-Rqst/index.tsx
@@ -1,7 +1,36 @@
 export const Mch_Withdraw_Rqst = reaxper(() => {
 	return <div>
-	
+		<WithdrawModal/>
 	
 	</div>;
 });
 
+export const WithdrawModal = reaxper(() => {
+	
+	const { Modal , Button , Input } = antd;
+	
+	return (
+		<Modal
+			title = { '确认提现信息' }
+			visible
+			closable = { false }
+			footer = { null }
+			width = { 380 }
+			className = { less.withdrawModal }
+		>
+			<div className = { less.contentContainer }>
+				<span className = { less.withdrawAmount }>商户提现金额</span>
+				<span className = { less.number }>
+					<span className = { less.unit }>R$</span>
+					500,000
+				</span>
+				<span className = { less.formInput }>转出USDT（TRC-20）</span>
+				<Input />
+				<Button type = "primary">提交</Button>
+				<Button type = "default">取消</Button>
+			</div>
+		</Modal>
+	);
+});
+
+import less from './index.module.less';
