28ce840181e4582fbce32fe472052f22acabd016
Kane <18299687659@163.com>
2022-11-24 14:16:53 +0800

    dayjs加入utils豪华套装

--- packages/overseas-payment-merchant-background-admin/src/Xcomponents/DatePickerUsingDayjs/index.tsx
+++ packages/overseas-payment-merchant-background-admin/src/Xcomponents/DatePickerUsingDayjs/index.tsx
@@ -1,8 +1,18 @@
-//@ts-nocheck
-
-import { Dayjs } from 'dayjs';
-import dayjsGenerateConfig from 'rc-picker/es/generate/dayjs';
+import {
+	Dayjs ,
+	extend,
+} from 'dayjs';
 import generatePicker from 'antd/es/date-picker/generatePicker';
+import Timezone from 'dayjs/plugin/timezone';
+import Utc from 'dayjs/plugin/utc';
+
+extend(Timezone);
+extend(Utc);
+
+import type { GenerateConfig } from 'rc-picker/es/generate';
+declare const dayjsGenerateConfig: GenerateConfig<Dayjs>;
+
+
 
 export const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig);
 
