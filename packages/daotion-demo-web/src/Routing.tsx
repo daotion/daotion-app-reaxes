7dda4d5ee5f97421acf4881ef434c9a8c241b2b1
Kane <18299687659@163.com>
2022-12-05 19:20:26 +0800

    新建daotion-demo-web项目

--- packages/daotion-demo-web/src/Routing.tsx
+++ packages/daotion-demo-web/src/Routing.tsx
@@ -3,27 +3,17 @@ import {
 	Routes ,
 	BrowserRouter ,
 } from 'react-router-dom';
-import { Test } from '@@pages/Test';
-
-import Homepage from '@@pages/Homepage';
-import ComingSoon from '@@pages/ComingSoon';
-import WhitePaper from '@@pages/WhitePaper';
-import NotFound from '@@pages/NotFound';
 
 
 export const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={toolkits.withOutlet(<Homepage />)} />
-
-        <Route path="/*" element={toolkits.withOutlet(<NotFound />)} />
-
-        <Route path="soon" element={toolkits.withOutlet(<ComingSoon />)} />
-
-        <Route path="paper" element={toolkits.withOutlet(<WhitePaper />)} />
-
-        <Route path="test/*" element={toolkits.withOutlet(<Test />)} />
+	      <Route
+		      path = "/*"
+	      >
+		      <Route index element={<>add a component here</>}/>
+	      </Route>
 
       </Routes>
     </BrowserRouter>
--- packages/daotion-demo-web/src/pages/Test/index.tsx
+++
@@ -1,5 +0,0 @@
-import {Test_Reaxel_i18n} from './reaxel-i18n';
-
-const Component = Test_Reaxel_i18n;
-
-export const Test = Component
